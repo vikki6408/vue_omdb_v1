d5cb3f30cab5c75a2d4d03c521dd42c937e1048f
Vikki6408 <vikki.dolt@eduvaud.ch>
2025-03-14 10:51:23 +0100

    start

---
+++ src/components/movieDetail.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="affichage_films">
+    <div class="infos_film">
+      <div id="image">
+        <img :src="Poster" alt="Blade runner" class="film">
+      </div>
+      <div id="infos">
+        <div id="rated">
+          <h1>{{Title}}</h1>
+          <h3 id="cadre">{{Rated}}</h3>
+          <h3><a id="getback" href="home.html">Get back</a></h3>
+
+        </div>
+        <h6>{{Released}}, {{Type}}, {{Runtime}}</h6>
+        <h6>{{Genre}}</h6>
+        <div class="vide"></div>
+        <h5>Director :  {{Director}}</h5>
+        <h5>Writer :  {{Writer}}</h5>
+        <h5>Actors :  {{Actors}}</h5>
+        <div class="vide"></div>
+        <h5>Description :</h5>
+        <h6>{{Plot}}</h6>
+      </div>
+    </div>
+    <div class="infos_film" style="margin-left: 10px;">
+      <div>
+        <h5>Languages :</h5>
+        <h6>{{Language}}</h6>
+        <h5>Country :</h5>
+        <h6>{{Country}}</h6>
+        <h5>Awards :</h5>
+        <h6>{{Awards}}</h6>
+        <div class="vide"></div>
+        <h5>Evaluations :</h5>
+        <!-- <ul class="ratings">
+          <li class="rating" v-for="rating in Ratings">{{rating.Source}} <br> {{rating.Value}}</li>
+        </ul> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String,
+    year: Number,
+    poster: String,
+  },
+}
+</script>
+
+<style scoped>
+body {
+  background-color: #274172;
+  font-size: 3vw;
+}
+
+div {
+  background-color: #274172;
+}
+.repartition{
+  display: flex;
+  height: 100vh;
+}
+.menu {
+  width: 12%;
+  height: auto;
+  background-color: #274172;
+  color: white;
+
+}
+.affichage_films {
+  background-color: #274172;
+  width: 88%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+  font-size: 3px;
+}
+
+.infos_film {
+  background-color: #274172;
+  color: white;
+  height: auto;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  gap: 35px;
+  font-size: 3px;
+
+}
+
+#image {
+  margin-left: 10px;
+  margin-top: 60px;
+  height: 55vh;
+}
+
+#infos {
+  margin-top: 50px;
+  height: auto;
+}
+
+.ratings {
+  display: flex;
+  flex-direction: row;
+}
+
+.rating {
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
+  margin-right: 30px;
+  font-size: 1.2vw;
+}
+
+.responsive {
+  width: 100%;
+  height: auto;
+}
+
+.film {
+  width: auto;
+  height: auto;
+}
+
+.vide {
+  height: 20px;
+}
+
+#rated {
+  display: flex;
+  justify-content: space-between;
+  color: aliceblue;
+
+}
+
+#getback{
+  color: #FFF56E;
+  margin-right: 10vh;
+}
+
+#cadre{
+  border: 2px solid #FFF56E;
+  height: 40px;
+  width: 55px;
+  margin-right: 80vh;
+  margin-top: 10px;
+  text-align: center;
+
+
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,61 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="repartition">
+      <div class="side-bar">
+        <img class="logo-image" src="../assets/logo.png" />
+
+        <div class="combobox">
+          <p>Type</p>
+          <select name="type" id="type">
+            <option value="film">Film</option>
+            <option value="tv-show">Tv-show</option>
+          </select>
+        </div>
+        <div class="combobox">
+          <p>Genre</p>
+          <select name="genre" id="genre">
+            <option>Action</option>
+            <option>Thriller</option>
+            <option>Documentary</option>
+          </select>
+        </div>
+        <div class="combobox">
+          <p>Years</p>
+          <select name="years" id="years">
+            <option>new generation</option>
+            <option>2010-2020</option>
+            <option>2000-2010</option>
+            <option>1990-2000</option>
+            <option>1980-1990</option>
+          </select>
+        </div>
+        <div class="combobox">
+          <p>Rating</p>
+          <select name="rating" id="rating">
+            <option>0-5</option>
+            <option>5-10</option>
+          </select>
+        </div>
+      </div>
+
+      <div id="display-home">
+        <div id="title-home">Bienvenue sur JEVISIS's films!</div>
+
+        <form id="search-container">
+          <div class="search">
+            <span class="material-symbols-outlined">search</span>
+            <input class="search-input" type="search" placeholder="Search.." />
+          </div>
+        </form>
+
+        <div id="displayMovies">
+          <HelloWorld :title="Title" :year="Year" :poster="Poster"/>
+          <HelloWorld :title="Title" :year="Year" :poster="Poster"/>
+          <HelloWorld :title="Title" :year="Year" :poster="Poster"/>
+          <HelloWorld :title="Title" :year="Year" :poster="Poster"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,7 +66,99 @@ import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+  data() {
+    return {
+      Title: `Blade Runner`,
+      Poster:
+          'https://m.media-amazon.com/images/M/MV5BOWQ4YTBmNTQtMDYxMC00NGFjLTkwOGQtNzdhNmY1Nzc1MzUxXkEyXkFqcGc@._V1_SX300.jpg',
+      Year: 1982,
+    }
+  },
 }
 </script>
+
+<style>
+#app {
+  margin: 0px;
+  padding: 0px;
+  background-color: #274172;
+  font-family: 'lexend', sans-serif;
+}
+
+.repartition {
+  height: 100%;
+  margin: 0px;
+  display: flex;
+}
+
+.side-bar {
+  height: 100vh;
+  width: fit-content;
+  display: flex;
+  flex-direction: column;
+  background-color: #8e949d;
+}
+
+.combobox {
+  display: flex;
+  flex-direction: column;
+  width: auto;
+  padding: 4%;
+}
+
+.logo-image {
+  height: 30%;
+  width: fit-content;
+}
+
+#display-home {
+  height: auto;
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+}
+
+#title-home {
+  text-align: center;
+  margin-top: 7%;
+  width: 100%;
+  font-size: 70px;
+  color: #fff56e;
+}
+
+#search-container {
+  width: auto;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 5%;
+  margin-bottom: 5%;
+}
+
+.search {
+  width: 400px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 14px;
+  border-radius: 28px;
+  background: #f6f6f6;
+}
+
+.search-input {
+  font-size: 16px;
+  font-family: 'lexend', sans-serif;
+  color: #333333;
+  margin-left: 14px;
+  outline: none;
+  border: none;
+  background: transparent;
+  width: 400px;
+}
+
+#displayMovies {
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
